23043ae911be375ed059866f0685f261bf41f788
Ziemnior <djpitugta@gmail.com>
2017-08-15 17:08:21 +0000

    Cleanup

--- sensor/discover.py
+++ sensor/discover.py
@@ -11,33 +11,30 @@ class NetworkConnection:
         self.interface = interface
         self.password = password
 
-    def discover_avalible_networks(self, interface=None):
+    def discover_avalible_networks(self):
         all_networks = []
-        interface = self.interface
-        avalible_networks = Cell.all(interface)
+        avalible_networks = Cell.all(self.interface)
         for network in avalible_networks:
             all_networks.append(network)
         return all_networks
 
-    def select_appropriate_networks(self, prefix=None):
+    def select_appropriate_networks(self):
         appropriate_network = namedtuple('network', 'ssid quality encrypted encryption_type')
         appropriate_networks = []
-        prefix = self.prefix
         for network in self.discover_avalible_networks():
-            if network.ssid.startswith(prefix):
+            if network.ssid.startswith(self.prefix):
                 appropriate_networks.append(
                     appropriate_network(network.ssid, network.quality, network.encrypted, network.encryption_type))
         return appropriate_networks
 
     def sort_appropriate_networks(self):
-        unsorted_appropriate_networks = self.select_appropriate_networks()
-        sorted_appropriate_networks = sorted(unsorted_appropriate_networks, key=attrgetter('quality'), reverse=True)
+        sorted_appropriate_networks = sorted(self.select_appropriate_networks(), key=attrgetter('quality'),
+                                             reverse=True)
         return sorted_appropriate_networks
 
     def connect_to_gateway(self, interface=None, password=None):
         interface = self.interface
         password = self.password
-        networks = self.sort_appropriate_networks()
         for network in self.sort_appropriate_networks():
             try:
                 print("Trying to connect to {}").format(network.ssid)
@@ -46,18 +43,20 @@ class NetworkConnection:
                 scheme.activate()
                 print("Connected to {}").format(network.ssid)
                 break
-	    except AssertionError:
-		scheme = Scheme.find(interface, network.ssid)
-		try:
-		    scheme.activate()
+            except AssertionError:
+                scheme = Scheme.find(interface, network.ssid)
+                try:
+                    scheme.activate()
                     print("Connected to {}").format(network.ssid)
                 except ConnectionError:
-		     raise e       
+                    raise e
             except ConnectionError as e:
                 print("Couldn't connect to {}").format(network.ssid)
                 scheme = Scheme.find(interface, network.ssid)
                 scheme.delete()
-		continue       
+                continue
+
+
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option('-p', '--prefix', dest='prefix', help="Specify gateway ssid prefix")
