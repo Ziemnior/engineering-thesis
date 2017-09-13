9fe40b8d1341be13e47d4a19220cb62dc8ced7e8
Ziemnior <djpitugta@gmail.com>
2017-09-13 19:18:14 +0200

    Add function to check who's at work currently

--- app/utils/registration.py
+++ app/utils/registration.py
@@ -7,3 +7,13 @@ def check_existing_uids():
     with create_session() as session:
         if not session.query(RecordUnregistered).all():
             flash("No cards to assign to user!", "warning")
+
+
+def return_uid(form):
+    if form.role.data == 'admin':
+        return None
+    else:
+        with create_session() as session:
+            session.query(RecordUnregistered).filter_by(user_id=form.user_id.data.user_id).delete()
+        return form.user_id.data.user_id
+
