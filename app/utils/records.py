d148618a317ccd3bcf5e136806dc985a12d2a53e
Jaskot <piotr.jaskot@nokia.com>
2017-09-20 17:51:23 +0200

    Add function to calculate time spent in work outside work hours

--- app/utils/records.py
+++ app/utils/records.py
@@ -9,14 +9,31 @@ def get_even_workdays(records):
 
 
 def get_workday_boundaries(user):
-    return [list(x) for x in zip(get_even_workdays(user)[::2], get_even_workdays(user)[1::2])]
+    return map(list, zip(get_even_workdays(user)[::2], get_even_workdays(user)[1::2]))
 
 
-def calculate_worktime(user):
+def calculate_usual_worktime(user):
     worktime = dict()
     i = 0
     for boundaries in get_workday_boundaries(user):
         business_time = BusinessHours(boundaries[1].timestamp, boundaries[0].timestamp)
-        worktime[i] = [business_time.get_minutes(), boundaries[0], boundaries[1]]
+        worktime[i] = [business_time.get_minutes(), boundaries[0].timestamp, boundaries[1].timestamp]
         i += 1
     return worktime
+
+
+def calculate_real_time(user):
+    real_time = dict()
+    i = 0
+    for boundaries in get_workday_boundaries(user):
+        real_time[i] = (boundaries[0].timestamp - boundaries[1].timestamp).seconds // 60
+        i += 1
+    return real_time
+
+
+def calculate_overtime(user):
+    overtime = dict()
+    real_time = calculate_real_time(user)
+    for key, value in calculate_usual_worktime(user).items():
+        overtime[key] = real_time[key] - value[0]
+    return overtime
